29cd3d19f86d5a2ba933e3a845fed5c24ff5d7e6
SzMarcell1200 <marcell1200@gmail.com>
2021-11-11 18:44:11 +0100

    0.0.20

--- index.php
+++ index.php
@@ -2,10 +2,7 @@
 include_once('./src/Acme/Mozi/Jegy.php');
 
 use Acme\Mozi\Jegy;
-<<<<<<< HEAD
 use Acme\Repter\Jegy2;
-=======
->>>>>>> b9b8b46646e6591ffa8db89685b6a703e9e35309
 
 $jegy = new Jegy('Free Guy', 990, 'Spielberg', 11, 4, '2021.11.11 15:45');
 $jegy2 = new Jegy("FR5190","Ryanair",25000,"Budapest Liszt Ferenc Nemzetközi Repülőtér (BUD)","Karol Wojtyła nemzetközi repülőtér (BRI)","2021.11.11 15:45");
