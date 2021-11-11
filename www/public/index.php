43deb0b8d989c9efb67458e039ba332e529b10d0
Vasicza Bálint <80480861+sicza95@users.noreply.github.com>
2021-11-11 19:52:11 +0100

    Update index.php
    merged index.php

--- www/public/index.php
+++ www/public/index.php
@@ -4,7 +4,9 @@ require('../src/Acme/Mozi/Jegy.php');
 use Acme\Mozi\Jegy;
 
 $jegy = new Jegy('Free Guy', 990, 'Spielberg', 11, 4, '2021.11.11 15:45');
+$jegy2 = new Jegy("FR5190","Ryanair",25000,"Budapest Liszt Ferenc Nemzetközi Repülőtér (BUD)","Karol Wojtyła nemzetközi repülőtér (BRI)","2021.11.11 15:45");
 
 ?>
 
 <pre><?= $jegy ?></pre>
+<pre><?= $jegy2 ?></pre>
