4295614b22cbe5baeda34c2432ea3c50fa55a66d
Vasicza Bálint <80480861+sicza95@users.noreply.github.com>
2021-11-11 20:01:20 +0100

    Update index.php
    error fix

--- www/public/index.php
+++ www/public/index.php
@@ -2,8 +2,8 @@
 require('../src/Acme/Mozi/Jegy.php');
 require('../src/Acme/Repter/Jegy.php');
 
-use Acme\Mozi\Jegy
-use Acme\Mozi\Jegy2
+use Acme\Mozi\Jegy;
+use Acme\Mozi\Jegy2;
 
 $jegy = new Jegy('Free Guy', 990, 'Spielberg', 11, 4, '2021.11.11 15:45');
 $jegy2 = new Jegy2("FR5190","Ryanair",25000,"Budapest Liszt Ferenc Nemzetközi Repülőtér (BUD)","Karol Wojtyła nemzetközi repülőtér (BRI)","2021.11.11 15:45");
