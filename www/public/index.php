439c5b45ea9146770c89491b6a0b97996ab893b8
Vasicza Bálint <80480861+sicza95@users.noreply.github.com>
2021-11-11 19:58:34 +0100

    Update index.php
    Fixed classname conflict

--- www/public/index.php
+++ www/public/index.php
@@ -1,10 +1,9 @@
 <?php
 require('../src/Acme/Mozi/Jegy.php');
+require('../src/Acme/Repter/Jegy.php');
 
-use Acme\Mozi\Jegy;
-
-$jegy = new Jegy('Free Guy', 990, 'Spielberg', 11, 4, '2021.11.11 15:45');
-$jegy2 = new Jegy("FR5190","Ryanair",25000,"Budapest Liszt Ferenc Nemzetközi Repülőtér (BUD)","Karol Wojtyła nemzetközi repülőtér (BRI)","2021.11.11 15:45");
+$jegy = new Acme\Mozi\Jegy('Free Guy', 990, 'Spielberg', 11, 4, '2021.11.11 15:45');
+$jegy2 = new Acme\Repter\Jegy("FR5190","Ryanair",25000,"Budapest Liszt Ferenc Nemzetközi Repülőtér (BUD)","Karol Wojtyła nemzetközi repülőtér (BRI)","2021.11.11 15:45");
 
 ?>
 
