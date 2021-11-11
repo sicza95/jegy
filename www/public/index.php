6072b810df96de127c4ff97f7dedcb6672fc1879
Vasicza Bálint <80480861+sicza95@users.noreply.github.com>
2021-11-11 20:00:09 +0100

    hobout now?

--- www/public/index.php
+++ www/public/index.php
@@ -2,8 +2,11 @@
 require('../src/Acme/Mozi/Jegy.php');
 require('../src/Acme/Repter/Jegy.php');
 
-$jegy = new Acme\Mozi\Jegy('Free Guy', 990, 'Spielberg', 11, 4, '2021.11.11 15:45');
-$jegy2 = new Acme\Repter\Jegy("FR5190","Ryanair",25000,"Budapest Liszt Ferenc Nemzetközi Repülőtér (BUD)","Karol Wojtyła nemzetközi repülőtér (BRI)","2021.11.11 15:45");
+use Acme\Mozi\Jegy
+use Acme\Mozi\Jegy2
+
+$jegy = new Jegy('Free Guy', 990, 'Spielberg', 11, 4, '2021.11.11 15:45');
+$jegy2 = new Jegy2("FR5190","Ryanair",25000,"Budapest Liszt Ferenc Nemzetközi Repülőtér (BUD)","Karol Wojtyła nemzetközi repülőtér (BRI)","2021.11.11 15:45");
 
 ?>
 
