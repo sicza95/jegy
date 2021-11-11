67a592ab453eea6e9b13662d49c77c27bad1fb0b
Balint Vasicza <sicza95@gmail.com>
2021-11-11 19:12:03 +0100

    attributes done\nconstructor done

--- src/Acme/Mozi/Jegy.php
+++ src/Acme/Mozi/Jegy.php
@@ -4,5 +4,20 @@ namespace Acme\Mozi;
 
 class Jegy
 {
+    private $cim; //Szöveg, a filmnek a címe.
+    private $ar; //Egész érték, forintban értelmezendő.
+    private $terem; //Szöveg, a terem neve, ahol a filmet vetítik.
+    private $sor; //Egész, a sor száma.
+    private $ules; //Egész, adott ülés száma a soron belül.
+    private $kezdes; //Szöveg, a film kezdetének időpontja, ami a dátumot és az időt is tartalmazza.
 
+    public function __construct($cim, $ar, $terem, $sor, $ules, $kezdes)
+    {
+        $this->cim = $cim;
+        $this->ar = $ar;
+        $this->terem = $terem;
+        $this->sor = $sor;
+        $this->ules = $ules;
+        $this->kezdes = $kezdes;
+    }
 }
